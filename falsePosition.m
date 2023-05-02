9f91f1500bdeb053323eea2b895e42463f588611
js-kaplan <132317724+js-kaplan@users.noreply.github.com>
2023-05-02 14:27:32 -0600

    False position algorithm

---
+++ falsePosition.m
@@ -0,0 +1,38 @@
+function [root, fx, ea, iter] = falsePosition(func, xl, xu, es, maxit, varargin)
+%falsePosition finds the root of a function using false position method
+if nargin < 3, (' 3 inputs required');
+end
+test = func(xl,varargin{:})*func(xu,varargin{:});
+if test > 0, error ('no sign change')
+end
+if nargin < 4 || isempty(es)
+    es = 0.0001;
+end
+if nargin < 5 || isempty(maxit)
+    maxit = 200;
+end
+ 
+iter = 0;
+xr = xl;
+ea = 100;
+
+while (1)
+    xr_old = xr; xr = (xu - (func(xu)*(xl-xu))/(func(xl)-func(xu)));
+    iter = iter +1;
+    if xr ~= 0, ea = abs((xr-xr_old)/xr)*100;
+    end
+    test = func(xl,varargin{:})*func(xr,varargin{:});
+    if test < 0
+        xu = xr;
+    elseif test > 0
+        xl = xr;
+    else 
+        ea = 0;
+    end
+    if ea <= es || iter >= maxit,break,end
+end
+
+root = xr;
+fx = func(xr,varargin{:});
+
+end
